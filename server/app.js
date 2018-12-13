8ddc2b09f5d66d5615f2b727e5134e7da8e0accb
mezleticia <chimezie.esiagu@booksrite.com>
2018-12-13 22:23:28 +0100

    update db

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-import babel_polyfill from 'babel-polyfill';
+// import babelPolyfill from 'babel-polyfill';
 import dotenv from 'dotenv';
 import logger from 'console';
 import express from 'express';
 import router from './Usingjs/routes';
-import incidentobj from './Usingjs/controllers/redFlagController';
+// import incidentobj from './Usingjs/controllers/redFlagController'
 import incidentdb from './Usingdb/controllers/incidents';
 import userdb from './Usingdb/controllers/user';
 import Auth from './Usingdb/middleware/Auth';
@@ -12,7 +12,8 @@ import Auth from './Usingdb/middleware/Auth';
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-const incident = process.env.TYPE === 'db' ? incidentdb : incidentobj;
+// const incident = process.env.TYPE === 'db' ? incidentdb : incidentobj;
+
 const app = express();
 
 app.use(express.json());
@@ -22,6 +23,7 @@ app.get('/', (req, res) => res.status(200).send({ message: 'Hello welcome to my 
 app.post('/api/v1/auth/signup', userdb.create);
 app.post('/api/v1/auth/login', userdb.login);
 app.post('/api/v1/incident', Auth.verifyToken, incidentdb.create);
+app.get('/api/v1/incident/me', Auth.verifyToken, incidentdb.getMyIncidents);
 app.get('/api/v1/incident', Auth.verifyToken, incidentdb.getAll);
 app.get('/api/v1/redflag', Auth.verifyToken, incidentdb.getRedFlag);
 app.get('/api/v1/intervention', Auth.verifyToken, incidentdb.getIntervention);
@@ -30,7 +32,7 @@ app.patch('/api/v1/incident/:id', Auth.verifyToken, incidentdb.update);
 app.patch('/api/v1/incident/:id/status', Auth.verifyToken, incidentdb.updateStatus);
 app.patch('/api/v1/incident/:id/comment', Auth.verifyToken, incidentdb.updateComment);
 app.patch('/api/v1/incident/:id/location', Auth.verifyToken, incidentdb.updateLocation);
-app.delete('/api/v1/incident/:id', Auth.verifyToken, incident.delete);
+app.delete('/api/v1/incident/:id', Auth.verifyToken, incidentdb.delete);
 
 
 app.listen(PORT, () => {
